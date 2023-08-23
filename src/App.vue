a6c1b0f7b1d8c42e3332448ec80ade6682df2c4c
PhoneKhaingHein <phone@infinitalk.co.jp>
2023-08-23 08:59:14 +0630

    version 1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <button @click="changeLocale('en')">English</button>
+      <button @click="changeLocale('ja')">Japan</button>
+      <p>{{ $t('message.greeting') }}</p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+   methods: {
+    changeLocale(locale) {
+      this.$i18n.locale = locale;
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createI18n } from 'vue-i18n';
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+    locale: 'en', // default locale
+    messages: {
+        en: {
+            message: {
+                greeting: 'Hello!'
+            }
+        },
+        ja: {
+            message: {
+                greeting: 'こんにちは！'
+            }
+        }
+        // Define other languages here
+    }
+});
+
+createApp(App).use(i18n).mount('#app');
